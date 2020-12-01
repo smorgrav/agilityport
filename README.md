431ef49c077f9112532c8b3876ffea9075b0e76f
smorgrav <torbjorn@smorgrav.org>
2020-12-01 15:03:15 +0100

    Add denali react library and document local development

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # Agility Port
 A denali react sample application using dog agility as the showcase.
 
+# Prerequisites 
+You need the denali-react library (currently in beta and not publicly available).
+
+There are two ways:
+1. Use the VerizonMedia npm registry 
+```
+npm set registry https://registry.npm.vzbuilders.com:4443/npm-registry
+```
+2. Use the VersionMedia internal repo: 
+```
+git clone git@git.vzbuilders.com:denali-system-language/denali-react-beta.git
+yarn link
+``` 
+
 # Starting from scratch
 * yarn create react-app agilityport --template typescript
+* yarn [add\\|link] @vzmi/denali-react-beta
+
+If you develop denali-react locally you need to run 
+```npm run-script build``` to make changes visible to this or other 
+projects using the linked npm library.
+
+
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {DnBox} from "@vzmi/denali-react-beta";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <DnBox className="additional--class" style={{width: '320px'}}>
+            <h2>Title</h2>
+            <p>YO</p>
+            <footer>
+                <p>This is a footer.</p>
+            </footer>
+        </DnBox>
     </div>
   );
 }
