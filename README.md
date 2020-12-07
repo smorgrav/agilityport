0bf2132e4f01bbafa8a85b06bddc2dea890c68ca
smorgrav <torbjorn@smorgrav.org>
2020-12-07 14:21:07 +0100

    Setup eslint and auto fix with google code style

--- README.md
+++ README.md
@@ -36,16 +36,16 @@ If you develop denali-react locally you need to run
 projects using the linked npm library.
 
 ## Scaffolding the app
-Starting from scratch we have some setup todo. Some of these are just perferences others are
-given by the choise of technology and libraries. 
+Starting from scratch we have some setup todo. Some of these are just preferences others are
+given by the choice of technology. 
 
 ### Install Denali
-The denali react library is only a convinience over the css framework. 
+The denali react library is only a convenience over the css framework. 
 So the css framework is a prerequisite.
  
 ``` yarn add denali-css```
 
-Typically you import this at the top level e.g. App.tsx with
+Typically, you import this at the top-level e.g. App.tsx with
 
 ```import denali-css/css/denali.css```
 
@@ -53,6 +53,28 @@ Typically you import this at the top level e.g. App.tsx with
 
 
 ### Configure linting and best practices
+I'm a big fan of mandating a specific code style and auto fix, format
+the code accordingly. This makes the code easier to read, diff and you avoid
+meaningless PR reviews about where to put whitespaces.
 
+```
+yarn add eslint
+yarn run eslint --init
+# Example choises of init wizard: enforce style, esm, react, 
+  typescript, browser, google style (guide) and JSON format
+```
+
+A small comparison of common styleguide can be found [here](https://medium.com/better-programming/comparing-the-top-three-style-guides-and-setting-them-up-with-eslint-98ea0d2fc5b7)
+
+To save you some gray hairs you can setup the auto fix feature on save in your favorite IDE
+
+* [Auto-eslint VSCode](https://www.digitalocean.com/community/tutorials/workflow-auto-eslinting)
+* [Auto-eslint Webstorm](https://www.jetbrains.com/help/webstorm/eslint.html#ws_js_eslint_activate) 
+
+As a final touch, we setup this auto-fix on git stage:
+
+```npx mrm lint-staged```
+
+This kind of forces the choosen style and best practices as far as is practical.
 
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
 import React, {useState} from 'react';
 import './App.css';
-import {DnBox, DnNavbar} from "@vzmi/denali-react-beta";
-import firebase from "firebase/app";
-import "firebase/analytics";
-import "firebase/auth";
-import "firebase/firestore";
-import 'denali-css/css/denali.css'
+import {DnBox, DnNavbar} from '@vzmi/denali-react-beta';
+import firebase from 'firebase/app';
+import 'firebase/analytics';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'denali-css/css/denali.css';
+
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC9vycrGHx4t8MrhzwgEUG9MOKVkJI_00k",
-    authDomain: "agilityport.firebaseapp.com",
-    databaseURL: "https://agilityport.firebaseio.com",
-    projectId: "agilityport",
-    storageBucket: "agilityport.appspot.com",
-    messagingSenderId: "1059309145754",
-    appId: "1:1059309145754:web:0b750cebbca8ed34619923",
-    measurementId: "G-D0Y3VL0NE5"
+  apiKey: 'AIzaSyC9vycrGHx4t8MrhzwgEUG9MOKVkJI_00k',
+  authDomain: 'agilityport.firebaseapp.com',
+  databaseURL: 'https://agilityport.firebaseio.com',
+  projectId: 'agilityport',
+  storageBucket: 'agilityport.appspot.com',
+  messagingSenderId: '1059309145754',
+  appId: '1:1059309145754:web:0b750cebbca8ed34619923',
+  measurementId: 'G-D0Y3VL0NE5',
 };
-//firebase.initializeApp(firebaseConfig);
-//const db = firebase.firestore();
+// firebase.initializeApp(firebaseConfig);
+// const db = firebase.firestore();
 
-  // Renable this when I have a skeleton of denali up and running
-   /*
+// Renable this when I have a skeleton of denali up and running
+/*
     db.collection("competitions").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             console.log("something3lse")
@@ -30,27 +31,29 @@ const firebaseConfig = {
     })*/
 
 
+// https://react-table.tanstack.com/docs/overview
+
 function App() {
-    const [competitions, setCompetitions] = useState({})
+  const [competitions, setCompetitions] = useState({});
   return (
-      <>
-          <DnNavbar>
-              <DnNavbar.Left>
-                <DnNavbar.Item>Left</DnNavbar.Item>
-              </DnNavbar.Left>
-              <DnNavbar.Responsive>
-              <DnNavbar.Center>
-                  <DnNavbar.Item>Center</DnNavbar.Item>
-              </DnNavbar.Center>
-              <DnNavbar.Right>
-                  <DnNavbar.Item>Right</DnNavbar.Item>
-              </DnNavbar.Right>
-              </DnNavbar.Responsive>
-          </DnNavbar>
-          <DnBox>
+    <>
+      <DnNavbar>
+        <DnNavbar.Left>
+          <DnNavbar.Item>Left</DnNavbar.Item>
+        </DnNavbar.Left>
+        <DnNavbar.Responsive>
+          <DnNavbar.Center>
+            <DnNavbar.Item>Center</DnNavbar.Item>
+          </DnNavbar.Center>
+          <DnNavbar.Right>
+            <DnNavbar.Item>Right</DnNavbar.Item>
+          </DnNavbar.Right>
+        </DnNavbar.Responsive>
+      </DnNavbar>
+      <DnBox>
             Hello
-          </DnBox>
-          </>
+      </DnBox>
+    </>
   );
 }
 
